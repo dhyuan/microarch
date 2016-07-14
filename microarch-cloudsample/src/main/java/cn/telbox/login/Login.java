9c188ceae426008dc03dad5c8959a5a4c632af9c
dhyuan <dhyuan@gmail.com>
2016-07-14 17:44:07 +0800

    Intgergrate the Swagger with Spring.

---
+++ microarch-cloudsample/src/main/java/cn/telbox/login/Login.java
@@ -0,0 +1,18 @@
+package cn.telbox.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by George on 2016/7/14.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Login {
+    private Long id;
+    private String loginId;
+    private String password;
+
+}
---
+++ microarch-cloudsample/src/main/java/cn/telbox/web/LoginController.java
@@ -0,0 +1,35 @@
+package cn.telbox.web;
+
+import cn.telbox.login.Login;
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by George on 2016/7/14.
+ */
+@RestController
+public class LoginController {
+    private final AtomicLong counter = new AtomicLong();
+
+    @ApiOperation(value = "login", nickname = "login", notes = "user login end-point.", response = Login.class)
+    @RequestMapping(method = RequestMethod.POST, path="/login", produces = "application/json")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "loginId", value = "User's loginId", required = true, dataType = "string", paramType = "form"),
+            @ApiImplicitParam(name = "password", value = "User's password", required = true, dataType = "string", paramType = "form")
+    })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = Login.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")})
+    public Login greeting(@ApiParam(name = "loginId", value = "user's login id.", required = true) @RequestParam(value="loginId") String loginId,
+                          @ApiParam(name = "password", value = "user's password.", required = true) @RequestParam(value="password") String password) {
+        return new Login(counter.incrementAndGet(), loginId, password);
+    }
+}
