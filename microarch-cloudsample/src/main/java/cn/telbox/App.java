c5c7293fb0d02626f7a98878db1d22ccb538d248
dhyuan <dhyuan@gmail.com>
2016-07-14 13:43:30 +0800

    Set up aggregation and parent pom. Use the spring-boot as the BOM, and load spring-cloud in the dependency management.

---
+++ microarch-cloudsample/src/main/java/cn/telbox/App.java
@@ -0,0 +1,13 @@
+package cn.telbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
